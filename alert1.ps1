ad70a655787a9896b94dd08ee7ea60741490533f
studentstestaccount <125878401+studentstestaccount@users.noreply.github.com>
2023-07-22 22:19:27 +0300

    Delete alert1.ps1

--- alert1.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-
-# Create a message box with "Hello, World!" message
-[System.Windows.Forms.MessageBox]::Show("Hello, World!", "PowerShell Alert", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
