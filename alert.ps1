0f6acc8d5becc6d95189eec99ad0976ea13da6cc
studentstestaccount <125878401+studentstestaccount@users.noreply.github.com>
2023-07-22 22:19:55 +0300

    Update alert.ps1

--- alert.ps1
+++ alert.ps1
@@ -5,4 +5,54 @@ Add-Type -AssemblyName System.Drawing
 [System.Windows.Forms.MessageBox]::Show("Hello, World!", "PowerShell Alert", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 
 
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$IP,
+
+    [Parameter(Mandatory = $true)]
+    [int]$Port
+)
+
+try {
+    $client = New-Object System.Net.Sockets.TcpClient
+    $client.Connect($IP, $Port)
+
+    $stream = $client.GetStream()
+    $reader = New-Object System.IO.StreamReader($stream)
+    $writer = New-Object System.IO.StreamWriter($stream)
+
+    # Create a new console for remote output
+    $remoteConsole = [System.Console]::OpenStandardOutput()
+    $remoteWriter = [System.IO.StreamWriter]::new($remoteConsole)
+
+    while ($true) {
+        $input = Read-Host
+        $writer.WriteLine($input)
+        $writer.Flush()
+
+        $output = $reader.ReadLine()
+        if ($output -eq $null) {
+            break
+        }
+
+        # Write remote console output to the local console
+        $remoteWriter.WriteLine($output)
+        $remoteWriter.Flush()
+    }
+}
+catch {
+    Write-Host "Failed to connect to the remote console."
+}
+finally {
+    if ($client -ne $null) {
+        $client.Close()
+    }
+
+    # Close the remote console
+    if ($remoteWriter -ne $null) {
+        $remoteWriter.Close()
+    }
+}
+
+
 
